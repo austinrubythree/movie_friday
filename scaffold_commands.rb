8ec2e7fcb317a15ad1b7b2c89757023db145f453
Austin <austin@gmail.com>
2019-08-09 13:15:58 -0700

    add devise, paperclip, bootstrap and style

---
+++ scaffold_commands.rb
@@ -0,0 +1,17 @@
+# rails g scaffold Movie title:string description:text movie_length:string director:string rating:string
+
+# rails g devise:install
+
+# rails g deivse:views
+# rails g devise User
+# rails db:migrate
+
+# rails g migration add_user_id_to_movies user_id:integer
+# rails db:migrate
+
+#upload image
+#install papaerclip
+
+#rails g paperclip movie image
+
+#layout create header partial
